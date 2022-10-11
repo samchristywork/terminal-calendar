03d9ff8b60522ff6cc9295bdbf75fa254a6cfe83
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-10-11 18:06:44 -0500

    Reverting change to MIN_SUPPORTED_VERSION.
    This change caused me to lose access to 1.0. Also added a comment to
    enforce a policy that MIN_SUPPORTED_VERSION should always be the initial
    major release.

--- src/version.h
+++ src/version.h
@@ -2,6 +2,6 @@
 #define VERSION_H
 
 #define TERMINAL_CALENDAR_VERSION "1.1.0"
-#define MIN_SUPPORTED_VERSION "1.1.0"
+#define MIN_SUPPORTED_VERSION "1.0.0" // Generally should be an X.0.0 release.
 
 #endif
