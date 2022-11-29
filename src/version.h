f07a4f0b0783a19cf1344ca91a163630eb137207
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-11-28 22:33:52 -0600

    Version increase: Updated metadata for rel 1.1.0
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/version.h
+++ src/version.h
@@ -2,10 +2,10 @@
 #define VERSION_H
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 0
+#define VERSION_MINOR 1
 #define VERSION_REVISION 0
-#define VERSION_STRING_SHORT "1.0.0"
-#define VERSION_STRING "terminal-calendar-1.0.0"
+#define VERSION_STRING_SHORT "1.1.0"
+#define VERSION_STRING "terminal-calendar-1.1.0"
 
 #define MIN_SUPPORTED_VERSION "1.0.0" // This should generally be a major release
 
