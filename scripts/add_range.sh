47ded9e1b1b8e00677fb35064573edd3b0734836
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-05 22:16:12 -0600

    Documentation: Added statement for ease of use
    This usage statement details how the program should be invoked. There is
    also a check to make sure the user specified the correct number of
    arguments.
    
    Contact: Sam Christy <samchristywork@gmail.com>

--- scripts/add_range.sh
+++ scripts/add_range.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+usage (){
+  echo "Usage: $(basename $0) filename"
+  echo
+  echo "The specified file should have one item for each line. Each item will be added"
+  echo "to the calendar on a new day starting the day after the script is run. Thus, if"
+  echo "the script is run on 2022-08-05, then the first line of the file will be added"
+  echo "to 2022-08-06, the second line to 2022-08-07, etc..."
+  exit 1
+}
+
+[ "$#" -eq "1" ] || usage
+
 i=1
 
 while read line; do
   terminal_calendar --cli append $(date -Idate -d "$i day") "$line"
   i=$((i+1))
-done < input.txt
+done < $1
