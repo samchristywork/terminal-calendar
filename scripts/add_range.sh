563d939cd448ead43d8c2bf37d99493a8982a495
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-10 08:04:00 -0600

    Feature: Added optional increment value to script
    The script now takes an "increment" parameter. This gives a bit more
    flexibility for scheduling tasks on a recurring basis.
    
    Contact: Sam Christy <samchristywork@gmail.com>

--- scripts/add_range.sh
+++ scripts/add_range.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 usage (){
-  echo "Usage: $(basename $0) filename"
+  echo "Usage: $(basename $0) filename [increment]"
   echo
   echo "The specified file should have one item for each line. Each item will be added"
   echo "to the calendar on a new day starting the day after the script is run. Thus, if"
   echo "the script is run on 2022-08-05, then the first line of the file will be added"
   echo "to 2022-08-06, the second line to 2022-08-07, etc..."
+  echo
+  echo "The optional increment value is specified after the filename which is"
+  echo "mandatory. The value defaults to 1. Any value other than a number will be"
+  echo "counted as zero. A value of 2 will skip 1 day for every item added to the"
+  echo "calendar, a value of 3 will skip 2 days, etc..."
   exit 1
 }
 
-[ "$#" -eq "1" ] || usage
+increment=1
+
+[ "$#" -eq "1" ] || { [ "$#" -eq "2" ] && { increment=$2; } } || usage
 
 i=1
 
 while read line; do
   terminal_calendar --cli append $(date -Idate -d "$i day") "$line"
-  i=$((i+1))
+  i=$((i+increment))
 done < $1
